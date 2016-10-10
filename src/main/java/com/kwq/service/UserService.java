9cd7a57770d862927c4f51d42a28fc7f98223c39
KevinBetterQ <1093850932@qq.com>
2016-10-10 22:21:20 +0800

    maven+spring+mybatis+mysql with error

---
+++ src/main/java/com/kwq/service/UserService.java
@@ -0,0 +1,16 @@
+package com.kwq.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kwq.domain.User;
+
+/**
+ * 功能概要：UserService接口类
+ * 
+ * @author linbingwen
+ * @since  2015年9月28日 
+ */
+public interface UserService {
+	User selectUserById(Integer userId);
+
+}
